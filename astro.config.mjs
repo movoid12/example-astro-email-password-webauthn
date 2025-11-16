d4ee014670cfd65816fc9f59292cb659885c30ef
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-11-16 11:44:54 +0100

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
-
-import node from "@astrojs/node";
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	adapter: node({
-		mode: "standalone"
-	}),
+	adapter: vercel(),
 	security: {
 		checkOrigin: true
 	}

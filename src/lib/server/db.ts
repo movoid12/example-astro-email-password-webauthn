62c8c2268c60eb8296e96b5b817679a3121a7983
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-11-16 12:20:11 +0100

    feat: integrate turso db instead of better-sqlite3

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,27 +1,32 @@
-import sqlite3 from "better-sqlite3";
-import { SyncDatabase } from "@pilcrowjs/db-query";
+import type { Row, ResultSet, InArgs } from "@libsql/client";
+import { turso } from "src/turso";
 
-import type { SyncAdapter } from "@pilcrowjs/db-query";
+interface Adapter {
+	query(statement: string, params: InArgs): Promise<Row[]>;
+	execute(statement: string, params: InArgs): Promise<ResultSet>;
+}
 
-const sqlite = sqlite3("sqlite.db");
-
-const adapter: SyncAdapter<sqlite3.RunResult> = {
-	query: (statement: string, params: unknown[]): unknown[][] => {
-		const result = sqlite
-			.prepare(statement)
-			.raw()
-			.all(...params);
-		return result as unknown[][];
+const adapter: Adapter = {
+	query: async (statement, params) => {
+		const { rows } = await turso.execute(statement, params);
+		return rows;
 	},
-	execute: (statement: string, params: unknown[]): sqlite3.RunResult => {
-		const result = sqlite.prepare(statement).run(...params);
+	execute: async (statement, params) => {
+		const result = await turso.execute(statement, params);
 		return result;
 	}
 };
 
-class Database extends SyncDatabase<sqlite3.RunResult> {
-	public inTransaction(): boolean {
-		return sqlite.inTransaction;
+class Database {
+	adapter: Adapter;
+	constructor(adapter: Adapter) {
+		this.adapter = adapter;
+	}
+	async query(statement: string, params: InArgs) {
+		return this.adapter.query(statement, params);
+	}
+	async execute(statement: string, params: InArgs) {
+		return this.adapter.execute(statement, params);
 	}
 }
 
